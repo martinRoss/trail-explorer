fcf6258dfb53b780920c579f4fc8cd36cd62031d
Tyler <tyler.r.martin23@gmail.com>
2017-11-28 11:00:32 -0500

    Bit of progress setting up the track projection

--- src/components/ThreeDMap.js
+++ src/components/ThreeDMap.js
@@ -1,6 +1,11 @@
 import React, { PureComponent } from 'react'
 import ReactDOM from 'react-dom'
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three'
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, Geometry } from 'three'
+import { geoMercator } from 'd3-geo'
+import bbox from '@turf/bbox'
+import length from '@turf/length'
+import center from '@turf/center'
+import { lineString } from '@turf/helpers'
 import Styled from 'styled-components'
 import constants from '../constants'
 
@@ -36,6 +41,7 @@ export default class ThreeDMap extends PureComponent {
   constructor(props) {
     super(props)
     this.setupScene = this.setupScene.bind(this)
+    this.updateSceneWithNewTrail = this.updateSceneWithNewTrail.bind(this)
   }
 
   /**
@@ -52,6 +58,35 @@ export default class ThreeDMap extends PureComponent {
   componentDidMount = () => this.setupScene()
 
   /**
+   * Update if new trail received
+   * @param {object} nextProps Next props
+   * @returns {void}
+   */
+  
+  componentWillReceiveProps(nextProps) {
+      if (this.props.selectedTrail !== nextProps.selectedTrail) this.updateSceneWithNewTrail()
+  }
+
+  updateSceneWithNewTrail = () => {
+    const { selectedTrail } = this.props
+    const geoJSON = JSON.parse(selectedTrail.geoJson)
+    const featBBox = bbox(geoJSON)
+    const [minX, minY, maxX, maxY] = featBBox
+    const latLength = length(lineString([[minX, minY], [minX, maxY]]), { units: 'miles' })
+    const longLength = length(lineString([[minX, minY], [maxX, minY]]), { units: 'miles' })
+    const maxLength = Math.max(latLength, longLength)
+    this.projection = geoMercator()
+      .translate(maxLength / 2, maxLength / 2)
+      .center(center(geoJSON))
+
+    // Center the camera
+    this.camera.position.set(0, -longLength / 2, latLength / 2);
+    // Setup THREE geometry
+    const trackGeom = new Geometry()
+  }
+  
+
+  /**
    * Sets up the scene for the 3d trail
    * @returns {void}
    */
@@ -59,24 +94,37 @@ export default class ThreeDMap extends PureComponent {
       this.scene = new Scene()
       this.camera = new PerspectiveCamera(fov, width / height, near, far)
       this.renderer = new WebGLRenderer()
+      //this.controls = new TrackballControls(this.camera)
       this.renderer.setSize(width, height)
       ReactDOM.findDOMNode(this.sceneDom).appendChild(this.renderer.domElement)
+
+      // Setup trail
+      this.updateSceneWithNewTrail()
+
+      const render = () => {
+          //this.controls.update()
+          requestAnimationFrame(render)
+          this.renderer.render(this.scene, this.camera)
+      }
+
+      render()
+
       // Just using sample code
-      this.geometry = new BoxGeometry( 1, 1, 1 )
-			this.material = new MeshBasicMaterial( { color: 0x00ff00 } )
-			this.cube = new Mesh(this.geometry, this.material)
-			this.scene.add(this.cube)
-			this.camera.position.z = 5
-			const animate = () => {
-				requestAnimationFrame(animate)
+      // this.geometry = new BoxGeometry( 1, 1, 1 )
+			// this.material = new MeshBasicMaterial( { color: 0x00ff00 } )
+			// this.cube = new Mesh(this.geometry, this.material)
+			// this.scene.add(this.cube)
+			// this.camera.position.z = 5
+			// const animate = () => {
+			// 	requestAnimationFrame(animate)
 
-				this.cube.rotation.x += 0.1
-				this.cube.rotation.y += 0.1
+			// 	this.cube.rotation.x += 0.1
+			// 	this.cube.rotation.y += 0.1
 
-				this.renderer.render(this.scene, this.camera)
-			}
+			// 	this.renderer.render(this.scene, this.camera)
+			// }
 
-			animate()
+			// animate()
   }
 
   /**
